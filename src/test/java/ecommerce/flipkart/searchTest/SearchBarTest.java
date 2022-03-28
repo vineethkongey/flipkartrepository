3c9528aa5a37fc37e5c688452895a1f18466fc3d
vineeth konge <vineeth konge@DESKTOP-MQQN6CU>
2022-03-28 12:36:21 +0530

    flipkart master commit

---
+++ src/test/java/ecommerce/flipkart/searchTest/SearchBarTest.java
@@ -0,0 +1,12 @@
+package ecommerce.flipkart.searchTest;
+
+public class SearchBarTest {
+	
+	public void entervalues() {
+		System.out.println("entered values properly");
+	}
+	
+	public void groceryValues() {
+		System.out.println("enterd grocery value");
+	}
+}
